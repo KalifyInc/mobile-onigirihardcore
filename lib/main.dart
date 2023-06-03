422b2b715040e96eab28e7e6a26915a7e2228c43
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-03 20:26:44 -0300

    feature:  created routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:OnigiriHardcore/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
-import 'app/view/home_page.dart';
+import 'app/view/pages/animes&hq_page.dart';
+import 'app/view/pages/home_page.dart';
+import 'app/view/pages/noticias_page.dart';
+import 'app/view/pages/csgo_tltv_news_page.dart';
 
 void main() => runApp(App());
 
@@ -17,8 +20,15 @@ class App extends StatelessWidget {
       title: appTitle,
       theme: theme.darkTheme,
       darkTheme: theme.darkTheme,
-      home: HomePage(title: appTitle),
+      initialRoute: '/home',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/home': (context) => const HomePage(),
+        '/noticias': (context) => const NoticiasPage(),
+        '/animes': (context) => const AnimesAndHQsPage(),
+        '/csgo': (context) => const CSGOHLTVNewsPage(),
+      },
+      home: const HomePage(),
     );
   }
 }
-
