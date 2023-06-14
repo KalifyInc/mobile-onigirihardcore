2300afdf305201c8a40b66ca8d7c375a5abae35e
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-14 20:55:16 -0300

    fix bug home

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,14 @@ import 'package:OnigiriHardcore/themes/app_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'app/config/firebase_options.dart';
 import 'app/view/pages/animes&hq_page.dart';
-import 'app/view/pages/error_page.dart';
 import 'app/view/pages/home_page.dart';
 import 'app/view/pages/intro_loading_page.dart';
 import 'app/view/pages/noticias_page.dart';
-import 'app/view/pages/csgo_tltv_news_page.dart';
 
-void main() => runApp(const App());
+void main() {
+  runApp(const App());
+}
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -21,10 +20,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final Future<FirebaseApp> _inicializacao = Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
   AppTheme theme = AppTheme();
 
   @override
@@ -40,22 +35,7 @@ class _AppState extends State<App> {
         '/home': (context) => const HomePage(),
         '/noticias': (context) => const NoticiasPage(),
         '/animes': (context) => const AnimesAndHQsPage(),
-        '/csgo': (context) => const CSGOHLTVNewsPage(),
       },
-      home: FutureBuilder(
-        future: _inicializacao,
-        builder: (context, app) {
-          if (app.connectionState == ConnectionState.done) {
-            return const IntroLoadingPage();
-          }
-
-          if (app.hasError) {
-            return const ErrorPage();
-          }
-
-          return const IntroLoadingPage();
-        },
-      ),
     );
   }
 }
