e2540c88326f8ffe6a07f593bf6eaebc9e484d4e
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-14 20:54:19 -0300

    feature: created intro loading page

--- lib/app/view/pages/intro_loading_page.dart
+++ lib/app/view/pages/intro_loading_page.dart
@@ -1,24 +1,37 @@
-import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:OnigiriHardcore/app/view/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import '../../config/firebase_options.dart';
+import 'error_page.dart';
 
-class IntroLoadingPage extends StatelessWidget {
+class IntroLoadingPage extends StatefulWidget {
   const IntroLoadingPage({super.key});
 
   @override
+  State<IntroLoadingPage> createState() => _IntroLoadingPageState();
+}
+
+class _IntroLoadingPageState extends State<IntroLoadingPage> {
+  final Future<FirebaseApp> _inicializacao = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return EasySplashScreen(
-      logo: Image.asset(
-        'assets/images/logo-transp.jpg',
-        width: 512,
-        height: 512,
-      ),
-      backgroundColor: Colors.grey.shade100,
-      showLoader: true,
-      loaderColor: Colors.lightBlue,
-      navigator: const HomePage(),
-      durationInSeconds: 5,
+    return FutureBuilder(
+      future: _inicializacao,
+      builder: (context, app) {
+        if (app.connectionState == ConnectionState.done) {
+          return const SplashPage();
+        }
+
+        if (app.hasError) {
+          return const ErrorPage();
+        }
+
+        return const SplashPage();
+      },
     );
   }
 }
