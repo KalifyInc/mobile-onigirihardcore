1fcd8da773d9cdff9d97cdb546fad2f207582c4b
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-17 13:52:25 -0300

    refactor: change the page to display the 15 firsts cards

--- lib/app/view/pages/home_page.dart
+++ lib/app/view/pages/home_page.dart
@@ -1,13 +1,10 @@
-import 'dart:async';
-
 import 'package:OnigiriHardcore/app/view/pages/loading_page.dart';
 import 'package:OnigiriHardcore/app/view/widgets/card_widget.dart';
 import 'package:OnigiriHardcore/app/view/widgets/drawer_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../controller/connectivity_controller.dart';
 import '../../service/posts_service.dart';
 import 'error_page.dart';
 
@@ -20,52 +17,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   PostsService firebaseService = PostsService();
-
-  late ConnectivityResult result;
-
-  late StreamSubscription subscription;
-
-  var isConnected = false;
+  var connectivityController = ConnectivityController();
 
   @override
   void initState() {
     super.initState();
-    startStreaming();
-  }
-
-  checkInternet() async {
-    result = await Connectivity().checkConnectivity();
-    if (result != ConnectivityResult.none) {
-      isConnected = true;
-    } else {
-      isConnected = false;
-      showDialogBox();
-    }
-    setState(() {});
-  }
-
-  showDialogBox() {
-    showDialog(
-        barrierDismissible: false,
-        context: context,
-        builder: (context) => CupertinoAlertDialog(
-                title: const Text('Sem internet!'),
-                content: const Text('Confira sua conexão com a internet'),
-                actions: [
-                  CupertinoButton.filled(
-                    child: const Text('Tente novamente'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                      checkInternet();
-                    },
-                  )
-                ]));
-  }
-
-  startStreaming() {
-    subscription = Connectivity().onConnectivityChanged.listen((event) async {
-      checkInternet();
-    });
+    connectivityController.startStreaming(context);
   }
 
   @override
@@ -76,7 +33,7 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: StreamBuilder<QuerySnapshot>(
-            stream: firebaseService.getAllSnapshots(),
+            stream: firebaseService.getFirstsSnapshots(),
             builder:
                 (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (snapshot.hasError) {
@@ -98,10 +55,8 @@ class _HomePageState extends State<HomePage> {
                         .map((doc) => CardWidget(
                               imageURL: doc['imageUrl'],
                               title: doc['title'],
-                              description: doc['description'],
                               categories: doc['categories'],
                               date: doc['lessDate'],
-                              author: doc['author'],
                             ))
                         .toList());
               } else if (snapshot.hasError) {
