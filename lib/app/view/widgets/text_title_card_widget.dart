2bc64a2f40d4f76c6e2510d945e66f7349a018ee
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-17 13:46:59 -0300

    feature: created text title card widget

---
+++ lib/app/view/widgets/text_title_card_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextTitleCardWidget extends StatefulWidget {
+  TextTitleCardWidget({super.key, required this.text});
+  var text;
+
+  @override
+  State<TextTitleCardWidget> createState() => _TextTitleCardWidgetState();
+}
+
+class _TextTitleCardWidgetState extends State<TextTitleCardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.text,
+      style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+    );
+  }
+}
