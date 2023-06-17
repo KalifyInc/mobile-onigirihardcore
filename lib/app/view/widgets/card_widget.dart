0530a080236c7ffefba82a2f02695baab6d2734d
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-17 13:49:37 -0300

    refactor: deleted the description and author from card

--- lib/app/view/widgets/card_widget.dart
+++ lib/app/view/widgets/card_widget.dart
@@ -1,18 +1,15 @@
+import 'package:OnigiriHardcore/app/view/widgets/text_title_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class CardWidget extends StatefulWidget {
   CardWidget(
       {super.key,
       required this.title,
-      required this.description,
-      required this.author,
       required this.categories,
       required this.date,
       required this.imageURL});
 
   var title;
-  var description;
-  var author;
   var categories;
   var date;
   var imageURL;
@@ -24,29 +21,33 @@ class CardWidget extends StatefulWidget {
 class _CardWidgetState extends State<CardWidget> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Image.network(widget.imageURL),
-              title: Text(widget.title),
-              subtitle: Text(widget.description),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+    return Card(
+      child: Column(
+        // mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: TextTitleCardWidget(text: widget.title),
+            trailing: Image.network(widget.imageURL,
+                width: 120, height: 120, fit: BoxFit.fill),
+            contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Text(widget.date),
-                const SizedBox(width: 8),
-                Text(widget.categories),
-                const SizedBox(width: 8),
-                Text(widget.author),
-                const SizedBox(width: 8),
+                Text(
+                  widget.date + ' - ',
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  widget.categories,
+                  style: const TextStyle(fontSize: 12),
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
