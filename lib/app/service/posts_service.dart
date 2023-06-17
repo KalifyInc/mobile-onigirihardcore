b878f6d0d1f47ff2403e39d3cf82628cf1b22ed1
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-17 13:54:19 -0300

    feature: add queryOrderByDate, getAllSnapshots etc

--- lib/app/service/posts_service.dart
+++ lib/app/service/posts_service.dart
@@ -5,14 +5,41 @@ class PostsService {
   final _db = FirebaseFirestore.instance;
 
   final FirebaseStorage storage = FirebaseStorage.instance;
-
   Query<Map<String, dynamic>> query() {
+    final query = _db.collection('posts');
+    return query;
+  }
+
+  Query<Map<String, dynamic>> queryOrderByDate() {
     final query = _db.collection('posts').orderBy('date', descending: true);
     return query;
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllSnapshots() {
-    final snapshots = _db.collection('posts').snapshots();
+    final snapshots = queryOrderByDate().snapshots();
+    return snapshots;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getFirstsSnapshots() {
+    final snapshots = queryOrderByDate().limit(15).snapshots();
+    return snapshots;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getAllMoviesSnapshots() {
+    final snapshots =
+        query().where('categories', isEqualTo: 'Movies').snapshots();
+    return snapshots;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getAllAnimesSnapshots() {
+    final snapshots =
+        query().where('categories', isEqualTo: 'Animes').snapshots();
+    return snapshots;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getAllGamesSnapshots() {
+    final snapshots =
+        query().where('categories', isEqualTo: 'Games').snapshots();
     return snapshots;
   }
 }
