a64cb8f4e7b35944de3e2f7171db3b0f32147ae3
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-17 13:48:36 -0300

    feature: created text title widget

---
+++ lib/app/view/widgets/text_title_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextTitleWidget extends StatefulWidget {
+  TextTitleWidget({super.key, required this.text});
+  var text;
+
+  @override
+  State<TextTitleWidget> createState() => _TextTitleWidgetState();
+}
+
+class _TextTitleWidgetState extends State<TextTitleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.text,
+      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+}
