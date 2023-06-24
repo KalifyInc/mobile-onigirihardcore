389f82fb2ec7331ff605d1fb7ed4206c1546704a
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-24 10:37:05 -0300

    refactor: add different querys

--- lib/app/service/posts_service.dart
+++ lib/app/service/posts_service.dart
@@ -1,45 +1,65 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 
 class PostsService {
   final _db = FirebaseFirestore.instance;
 
   final FirebaseStorage storage = FirebaseStorage.instance;
-  Query<Map<String, dynamic>> query() {
-    final query = _db.collection('posts');
-    return query;
-  }
 
-  Query<Map<String, dynamic>> queryOrderByDate() {
+  Query<Map<String, dynamic>> query() {
     final query = _db.collection('posts').orderBy('date', descending: true);
     return query;
   }
 
+  Query<Map<String, dynamic>> queryAnimes() {
+    final queryAnimes = query().where('categories', isEqualTo: 'Animes');
+    return queryAnimes;
+  }
+
+  Query<Map<String, dynamic>> queryMovies() {
+    final queryMovies = query().where('categories', isEqualTo: 'Movies');
+    return queryMovies;
+  }
+
+  Query<Map<String, dynamic>> queryGames() {
+    final queryGames = query().where('categories', isEqualTo: 'Games');
+    return queryGames;
+  }
+
+  Query<Map<String, dynamic>> queryTechnologies() {
+    final queryTechnologies =
+        query().where('categories', isEqualTo: 'Technologies');
+    return queryTechnologies;
+  }
+
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllSnapshots() {
-    final snapshots = queryOrderByDate().snapshots();
+    final snapshots = query().snapshots();
     return snapshots;
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getFirstsSnapshots() {
-    final snapshots = queryOrderByDate().limit(15).snapshots();
+    final snapshots = query().limit(15).snapshots();
     return snapshots;
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllMoviesSnapshots() {
-    final snapshots =
-        query().where('categories', isEqualTo: 'Movies').snapshots();
+    final snapshots = queryMovies().snapshots();
     return snapshots;
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllAnimesSnapshots() {
-    final snapshots =
-        query().where('categories', isEqualTo: 'Animes').snapshots();
+    final snapshots = queryAnimes().snapshots();
     return snapshots;
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getAllGamesSnapshots() {
-    final snapshots =
-        query().where('categories', isEqualTo: 'Games').snapshots();
+    final snapshots = queryGames().snapshots();
+    return snapshots;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getAllTechnologiesSnapshots() {
+    final snapshots = queryTechnologies().snapshots();
     return snapshots;
   }
 }
