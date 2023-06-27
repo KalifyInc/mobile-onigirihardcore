02cafba88768880c3d637fa307487a4aa0665ae1
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-27 19:08:37 -0300

    feature: created compartilhar button

--- lib/app/view/pages/card_inside_page.dart
+++ lib/app/view/pages/card_inside_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+
+import '../../controller/open_link_controller.dart';
 
 class CardInsidePage extends StatelessWidget {
   CardInsidePage({
@@ -12,16 +15,20 @@ class CardInsidePage extends StatelessWidget {
     required this.date,
     required this.bodyPost,
     required this.detailDate,
+    required this.slug,
   });
 
   final String? image;
   final String imageURL;
-  final String author;
+  final String? author;
   final String title;
-  final String categories;
-  final String date;
+  final String? categories;
+  final String? date;
   final String bodyPost;
   final String detailDate;
+  final String slug;
+
+  final controller = OpenLinkController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +39,16 @@ class CardInsidePage extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16.0),
-                      image: DecorationImage(
-                          fit: BoxFit.cover, image: NetworkImage(imageURL)))),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(imageURL),
+                  ),
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.all(24.0),
                 child: Column(
@@ -58,6 +69,22 @@ class CardInsidePage extends StatelessWidget {
                       shrinkWrap: true,
                     ),
                     const SizedBox(height: 12),
+                    TextButton.icon(
+                        onPressed: () async =>
+                            await Clipboard.setData(ClipboardData(
+                                    text:
+                                        "https://onigirihardcore.com.br/$slug"))
+                                .then((_) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text(
+                                          "Link copiado na área de transferência")));
+                            }),
+                        label: const Text(
+                          'Compartilhar',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        icon: const Icon(Icons.share)),
                   ],
                 ),
               ),
