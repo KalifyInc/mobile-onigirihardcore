ad088c044cb4a383af924262417afade3dc445fb
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-29 18:00:35 -0300

    feature: add bottom tabs on app

--- lib/app/view/pages/splash_page.dart
+++ lib/app/view/pages/splash_page.dart
@@ -1,7 +1,6 @@
+import 'package:OnigiriHardcore/app/view/pages/bottom_tabs_page.dart';
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
-
-import 'home_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -22,7 +21,7 @@ class _SplashPageState extends State<SplashPage> {
       backgroundColor: Colors.grey.shade100,
       showLoader: true,
       loaderColor: Colors.lightBlue,
-      navigator: const HomePage(),
+      navigator: BottomTabsPage(selectedIndex: 0),
       durationInSeconds: 10,
     );
   }
