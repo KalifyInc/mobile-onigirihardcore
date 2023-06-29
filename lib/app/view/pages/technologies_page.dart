46e643bb0eaa9ec35dfd0fef3f4d391ddf4520f4
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-29 18:00:55 -0300

    refactor: deleted drawer

--- lib/app/view/pages/technologies_page.dart
+++ lib/app/view/pages/technologies_page.dart
@@ -1,7 +1,6 @@
 import 'package:OnigiriHardcore/app/controller/connectivity_controller.dart';
 import 'package:OnigiriHardcore/app/view/pages/loading_page.dart';
 import 'package:OnigiriHardcore/app/view/widgets/card_widget.dart';
-import 'package:OnigiriHardcore/app/view/widgets/drawer_widget.dart';
 import 'package:OnigiriHardcore/app/view/widgets/text_title_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_pagination/firebase_pagination.dart';
@@ -31,7 +30,6 @@ class _TechnologiesPageState extends State<TechnologiesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Onigiri Hardcore'), centerTitle: true),
-      drawer: const DrawerWidget(),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
