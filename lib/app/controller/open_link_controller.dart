bf7f070d778330f0e36ebdd65e73651644d9a2fb
Helen Cris <helencrisfernandes@hotmail.com>
2023-08-01 19:12:50 -0300

    refactor: add message error

--- lib/app/controller/open_link_controller.dart
+++ lib/app/controller/open_link_controller.dart
@@ -1,12 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class OpenLinkController {
-  openLink(urlParam) async {
+  openLink({required BuildContext context, required String urlParam}) async {
     Uri url = Uri.parse(urlParam);
     try {
       await launchUrl(url, mode: LaunchMode.externalApplication);
     } catch (e) {
-      print('Error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Erro: ${e.toString()}',
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.redAccent,
+        ),
+      );
     }
   }
 }
